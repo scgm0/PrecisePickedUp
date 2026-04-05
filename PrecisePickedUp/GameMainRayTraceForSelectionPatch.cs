4fd8f6d70506140c0ddb212ceb32444362d388bf
scgm0 <2682963017@qq.com>
2026-04-06 05:15:08 +0800

    fix: 让左键按住时不改变光线投射结果，掉落物不会阻挡左键操作

--- PrecisePickedUp/GameMainRayTraceForSelectionPatch.cs
+++ PrecisePickedUp/GameMainRayTraceForSelectionPatch.cs
@@ -1,6 +1,8 @@
+using System;
 using HarmonyLib;
 using Vintagestory.API.Common;
 using Vintagestory.API.MathTools;
+using Vintagestory.Client.NoObf;
 using Vintagestory.GameContent;
 
 namespace PrecisePickedUp;
@@ -8,11 +10,17 @@ namespace PrecisePickedUp;
 public class GameMainRayTraceForSelectionPatch {
 
 	[HarmonyPriority(Priority.First)]
-	public static void PreFix(ref EntityFilter efilter) {
+	public static void PreFix(IWorldIntersectionSupplier supplier, ref EntityFilter efilter) {
 		if (efilter is null || !efilter.Method.Name.StartsWith("<UpdateCurrentSelection>")) {
 			return;
 		}
 
+		if (supplier is ClientMain clientMain) {
+			var api = clientMain.Api;
+			if (clientMain.MouseStateRaw.Left) {
+				return;
+			}
+		}
 		var oldFilter = efilter;
 		efilter = e => e is EntityItem or EntityProjectile { NonCollectible: false } ||
 			PrecisePickedUpModSystem.EnableOverhaulCompat && OverhaulCompat.RayTraceForSelection(e) ||
