82c7e0c04e1e125b2f31ad16f5a8dad33e770216
scgm0 <2682963017@qq.com>
2026-05-06 21:36:33 +0800

    refactor: 优化物品合并逻辑与变量命名

--- PrecisePickedUp/EntityItemBehavior.cs
+++ PrecisePickedUp/EntityItemBehavior.cs
@@ -13,7 +13,7 @@ namespace PrecisePickedUp;
 public sealed class EntityItemBehavior(Entity entity) : EntityBehavior(entity) {
 
 	static private readonly string ActionLangCode = Lang.Get($"precisepickedup:{nameof(EnumDespawnReason.PickedUp)}");
-	private float cumulativeTime;
+	private float _cumulativeTime;
 	public override string PropertyName() { return nameof(EntityItemBehavior); }
 
 	public override void OnGameTick(float deltaTime) {
@@ -21,45 +21,54 @@ public sealed class EntityItemBehavior(Entity entity) : EntityBehavior(entity) {
 			return;
 		}
 
-		cumulativeTime += deltaTime;
-		if (cumulativeTime < PrecisePickedUpModSystem.Config.MergeInterval) {
+		_cumulativeTime += deltaTime;
+		if (_cumulativeTime < PrecisePickedUpModSystem.Config.MergeInterval) {
 			return;
 		}
 
-		cumulativeTime = 0;
-		var item = (EntityItem)entity;
-		if (item.Slot.Itemstack == null || !item.Collided) {
+		_cumulativeTime = 0;
+		var entityItem = (EntityItem)entity;
+		if (entityItem.Slot.Itemstack == null) {
 			return;
 		}
 
-		if (item.Slot.Itemstack.StackSize <= 0) {
-			item.Die(EnumDespawnReason.Expire);
-			item.WatchedAttributes.SetInt("stackCount", 0);
+		var currentSize = entityItem.Slot.Itemstack.StackSize;
+		if (entityItem.Slot.Itemstack.StackSize <= 0) {
+			entityItem.Die(EnumDespawnReason.Expire);
+			entityItem.WatchedAttributes.SetInt("stackCount", 0);
 			return;
 		}
 
-		var quantity = item.Slot.Itemstack.Collectible.MaxStackSize - item.Slot.Itemstack.StackSize;
+		var quantity = entityItem.Slot.Itemstack.Collectible.MaxStackSize - currentSize;
 		if (quantity <= 0) {
 			return;
 		}
 
-		foreach (var entity1 in item.Api.World.GetEntitiesAround(item.Pos.XYZ,
+		var entities = entityItem.Api.World.GetEntitiesAround(entityItem.Pos.XYZ,
 			PrecisePickedUpModSystem.Config.MergeRange.X,
 			PrecisePickedUpModSystem.Config.MergeRange.Y,
 			e => e is EntityItem {
-				Collided: true, Slot: { } slot
-			} && slot.StackSize <= item.Slot.StackSize && e != item)) {
-			var entityItem = (EntityItem)entity1;
-			quantity -= entityItem.Slot.TryPutInto(item.Api.World, item.Slot, quantity);
-			item.WatchedAttributes.SetInt("stackCount", item.Slot.Itemstack.StackSize);
-			if (entityItem.Slot.Itemstack is not { StackSize: > 0 }) {
-				entityItem.Die(EnumDespawnReason.Expire);
-				entityItem.WatchedAttributes.SetInt("stackCount", 0);
+					Slot: {
+						Itemstack: { } itemStack
+					} slot
+				} && e != entityItem && itemStack.Equals(entityItem.Api.World, entityItem.Slot.Itemstack) &&
+				slot.StackSize <= entityItem.Slot.StackSize);
+
+		foreach (var entity1 in entities) {
+			var entityItem2 = (EntityItem)entity1;
+			quantity -= entityItem2.Slot.TryPutInto(entityItem.Api.World, entityItem.Slot, quantity);
+
+			if (entityItem2.Slot.Itemstack is not { StackSize: > 0 }) {
+				entityItem2.Die(EnumDespawnReason.PickedUp);
 			}
 
 			if (quantity <= 0) {
 				break;
 			}
+		}
+
+		if (currentSize != entityItem.Slot.Itemstack.StackSize) {
+			entityItem.WatchedAttributes.SetInt("stackCount", entityItem.Slot.Itemstack.StackSize);
 		}
 	}
 
@@ -84,7 +93,7 @@ public sealed class EntityItemBehavior(Entity entity) : EntityBehavior(entity) {
 			return;
 		}
 
-		var itemStack = ((EntityItem)entity).Slot.Itemstack;
+		var itemStack = ((EntityItem)entity).Slot.Itemstack!;
 		var entities = entity.Api.World.GetEntitiesAround(entity.Pos.XYZ,
 			PrecisePickedUpModSystem.Config.PickupRange.X,
 			PrecisePickedUpModSystem.Config.PickupRange.Y,
@@ -95,7 +104,7 @@ public sealed class EntityItemBehavior(Entity entity) : EntityBehavior(entity) {
 	}
 
 	public void OnCollideWithPlayer(EntityPlayer player) {
-		var collect = (EntityBehaviorCollectEntities)player.GetBehavior("collectitems");
+		var collect = (EntityBehaviorCollectEntities)player.GetBehavior("collectitems")!;
 		collect.OnFoundCollectible(entity);
 		var item = (EntityItem)entity;
 		if (item.Slot.Itemstack is not { StackSize: > 0 }) {
